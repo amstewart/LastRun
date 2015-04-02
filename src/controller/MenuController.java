d6a19c69ee60c22734c7cb513c0b98f7453de56c
tessbianchi <tesshbianchi@gmail.com>
2015-04-02 17:40:57 -0400

    Merge branch 'Tess'

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -3,12 +3,19 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
+
 import model.GameBundle;
 import state.StateMachine;
 import view.viewport.MenuViewport;
 
 
-public class MenuController extends Controller {
+public class MenuController extends Controller{
 
     private MenuViewport view;
 
@@ -29,6 +36,13 @@ public class MenuController extends Controller {
   
     private void addActionListeners(){
         view.continueButton.addActionListener(goToNewGame);
+        view.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0), "forward");
+        view.getActionMap().put("forward", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test");
+            }
+        });
     }
     
 }
