eae66b5f45f0644667de05cc03ec2bb2f185eeeb
amstewart <diacopium@gmail.com>
2015-04-07 16:36:13 -0400

    More entity fun. Added the Describable interface.

--- src/model/entity/Entity.java
+++ src/model/entity/Entity.java
@@ -1,10 +1,11 @@
 package model.entity;
 
+import model.Describable;
 import model.inventory.EquippedInventoryManager;
 import model.items.Inventory;
 import java.util.LinkedList;
 
-public class Entity {
+public abstract class Entity implements Describable {
 
     // Fields
     private EquippedInventoryManager equipment = new EquippedInventoryManager();
@@ -13,4 +14,15 @@ public class Entity {
     private Stats stats_mod = new Stats();
 	private LinkedList<Status> statuses = new LinkedList<>();
 	private Inventory inventory = new Inventory();
+
+    public void addStatus(Status status) {
+        if (!statuses.contains(status))
+            statuses.push(status);
+    }
+
+    public String getName() { return this.name; }
+
+    public boolean removeStatus(Status status) {
+        return statuses.remove(status);
+    }
 }
