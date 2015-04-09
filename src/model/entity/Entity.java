95048f3cea19c395b0a5e698cc497c03c342cee7
tessbianchi <tesshbianchi@gmail.com>
2015-04-09 17:28:54 -0400

    Entity

--- src/model/entity/Entity.java
+++ src/model/entity/Entity.java
@@ -4,19 +4,75 @@
  * and open the template in the editor.
  */
 package model.entity;
+import model.Describable;
 
+import java.util.LinkedList;
 
-import model.item.Inventory;
+import model.enums.DefinedStats;
+import model.item.CoinPouch;
 
 
 public class Entity {
-	BagOfMoney bagOfMoney;
+	CoinPouch coinPouch;
 	Status status;
-	private Inventory inventory;
+    // Fields
+    private String name = "NONAME";
+    private Stats stats;
+    private Stats saving_stats = DefinedStats.ENTITYSTATS.getStats();
+	private LinkedList<Status> statuses = new LinkedList<>();
+	private Inventory inventory = new Inventory();
 
 	public Entity() {
-		inventory = new Inventory();
+		super();
+		this.stats = saving_stats;
 	}
+	public void mergeStats(Stats stat) {
+		stats.mergeStats(stat);
+	}
+	public void unMergeStats(Stats stat) {
+		stats.unMergeStats(stat);
+	}
+	public void setLivesLeft(int livesLeft) {
+		stats.setLivesLeft(livesLeft);
+	}
+
+	public void setStrength(int strength) {
+		stats.setStrength(strength);
+	}
+
+	public void setAgility(int agility) {
+		stats.setAgility(agility);
+	}
+
+	public void setIntellect(int intellect) {
+		stats.setIntellect(intellect);
+	}
+
+	public void setHardiness(int hardiness) {
+		stats.setHardiness(hardiness);
+	}
+
+	public void setExperience(int experience) {
+		stats.setExperience(experience);
+	}
+
+	public void setMovement(int movement) {
+		stats.setMovement(movement);
+	}
+
+	public void setEquippedArmor(int equippedArmor) {
+		stats.setEquippedArmor(equippedArmor);
+	}
+
+	public void setEquippedWeapon(int equippedWeapon) {
+		stats.setEquippedWeapon(equippedWeapon);
+	}
+	
+    public void addStatus(Status status) {
+        if (!statuses.contains(status))
+            statuses.push(status);
+    }
+
 
 	public Inventory getInventory() {
 		return inventory;
