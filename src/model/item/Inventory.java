f4f29efd7e7d03abc9c5962f3fa9350995e82d9c
Ron Quan <rquan24@ufl.edu>
2015-04-09 17:23:21 -0400

    readded inventory and item

---
+++ src/model/item/Inventory.java
@@ -0,0 +1,37 @@
+package model.item;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Inventory {
+    private ArrayList<TakeableItem> items;
+
+    public Inventory() {
+        items = new ArrayList<TakeableItem>();
+    }
+
+    public boolean addItem(TakeableItem item) {
+        if(item != null) {
+            return items.add(item);
+        }
+        return false;
+    }
+
+    public boolean removeItem(TakeableItem item) {
+        if(items.contains(item)) {
+            return items.remove(item);
+        }
+        return false;
+    }
+
+    public TakeableItem[] getItems() {
+        Collections.sort(items, new Comparator<TakeableItem>() {
+            public int compare(TakeableItem item1, TakeableItem item2) {
+                return item1.getName().compareTo(item2.getName());
+            }
+        });
+
+        return items.toArray(new TakeableItem[items.size()]);
+    }
+}
