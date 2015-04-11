003eefd0509790e0ea916cfd0638eb75f54394cf
amstewart <diacopium@gmail.com>
2015-04-11 14:20:30 -0400

    Had Vehicle extend NPC

--- src/model/entity/vehicle/Vehicle.java
+++ src/model/entity/vehicle/Vehicle.java
@@ -1,10 +1,15 @@
 
 package model.entity.vehicle;
 
+import model.entity.npc.NPC;
+
 /**
  *
  * @author ChrisMoscoso
  */
-public class Vehicle {
-    
+public class Vehicle extends NPC {
+
+    public Vehicle (String art_asset) {
+        super(art_asset);
+    }
 }
