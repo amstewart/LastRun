77680550e1139fa971ca49f36257f5c321a6b20c
Chris Moscoso <christophermoscoso@gmail.com>
2015-04-11 16:55:51 -0400

    Added GetEntities function to GameMap

---
+++ src/view/viewport/InventoryViewport.java
@@ -0,0 +1,25 @@
+
+package view.viewport;
+
+import java.util.ArrayList;
+import model.action.Action;
+
+/**
+ *
+ * @author ChrisMoscoso
+ */
+public class InventoryViewport extends Viewport{
+    public InventoryViewport(){
+        
+    }
+    
+    @Override
+    public void render() {
+        
+    }
+
+    @Override
+    public void setListeners(ArrayList<Action> a) {
+        
+    }
+}
