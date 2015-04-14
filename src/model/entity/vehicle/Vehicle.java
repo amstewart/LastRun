87b25e0e4627e4994a27212ce62d7dcab13b7537
amstewart <diacopium@gmail.com>
2015-04-13 20:11:47 -0400

    Added descriptions for vehicles.

--- src/model/entity/vehicle/Vehicle.java
+++ src/model/entity/vehicle/Vehicle.java
@@ -1,15 +1,18 @@
-
 package model.entity.vehicle;
 
 import model.entity.npc.NPC;
 
-/**
- *
- * @author ChrisMoscoso
- */
 public class Vehicle extends NPC {
 
-    public Vehicle (String art_asset) {
+    public Vehicle (String art_asset, String name) {
         super(art_asset);
+        setName(name);
+    }
+
+    public String getDescription() {
+        String ret = "It's a ";
+        ret += getName();
+        ret += ". You can ride it.";
+        return ret;
     }
 }
