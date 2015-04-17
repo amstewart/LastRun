00c697b65ccb96783a9a1ee291907c15c80eae59
amstewart <diacopium@gmail.com>
2015-04-17 17:57:51 -0400

    Implemented SpellBook.add

--- src/model/item/equipment/SpellBook.java
+++ src/model/item/equipment/SpellBook.java
@@ -1,11 +1,25 @@
 package model.item.equipment;
 
+import model.effect.Spell;
 import model.item.SummonerEquipmentManager;
 
+import java.util.HashSet;
+
 public class SpellBook extends SummonerEquipment {
-    // TODO: ADD spell
+
+    private HashSet<Spell> spells = new HashSet<>();
+
     public SpellBook(String name, int value) {
         super(name, value);
+    }
+
+    /**
+     * Adds a provided spell object to this spellbook
+     * @param new_spell The new Spell to add
+     * @return True, if the new spell was added; false, if the spell was already present in the spellbook
+     */
+    public boolean addSpell(Spell new_spell) {
+        return spells.add(new_spell);
     }
 
     @Override
