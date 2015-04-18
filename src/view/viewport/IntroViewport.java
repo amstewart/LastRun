49eff958c9f2466b09ff01c33fd8800266fd528a
carlos.vizcaino@ufl.edu <dv112358>
2015-04-18 10:55:38 -0400

    Itro View is Done

--- src/view/viewport/IntroViewport.java
+++ src/view/viewport/IntroViewport.java
@@ -9,7 +9,11 @@ import controller.action.Action;
 import controller.action.stateMachineAction.GoBackAction;
 import controller.action.stateMachineAction.GoToGameAction;
 import java.awt.Graphics;
-import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+import java.util.Queue;
 import javax.swing.JButton;
 import model.entity.occupation.Occupation;
 import utility.ImageUtil;
@@ -24,25 +28,28 @@ public class IntroViewport extends Viewport {
      * Creates new form IntroViewport
      */
     Occupation occupation;
+    Queue<String> queue = new LinkedList<String>();
     
     public IntroViewport(Occupation occupation) {
         this.occupation = occupation;
         initComponents();
-        backgroundImage = new ImageIcon(ImageUtil.INTRO_BACKGROUND);
         addActionListeners();
         setButtonTransparent(goToCharacterSelectionButton);
         setButtonTransparent(goToGame);
+        setStorieToList();
     }
     
      @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-         g.drawImage(backgroundImage.getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
+         
+         Image image = ImageUtil.getImage(ImageUtil.INTRO_BACKGROUND, this.getWidth(), this.getHeight()).getImage();
+         g.drawImage(image, 0, 0, this);
     }
     
     private void addActionListeners(){
         goToCharacterSelectionButton.addActionListener(Action.getActionListener(new GoBackAction()));
-        goToGame.addActionListener(Action.getActionListener(new GoToGameAction()));
+        goToGame.addActionListener(continueButtonAction);
     }
     
     private void setButtonTransparent(JButton button){
@@ -51,6 +58,31 @@ public class IntroViewport extends Viewport {
         button.setContentAreaFilled(false);
         button.setBorderPainted(false);
     }
+    
+    private void setStorieToList(){
+        
+        queue.add("Phase 1");
+        queue.add("Phase 2");
+        queue.add("Phase 3");
+    }
+    
+    private ActionListener continueButtonAction = new ActionListener(){
+    
+        @Override
+        public void actionPerformed(ActionEvent e){
+            
+            String storyLine = queue.poll();
+            if (storyLine == null){
+                
+                Action action = new GoToGameAction();
+                action.perform();
+            }
+            else {
+                
+                storyLineLabel.setText(storyLine);
+            }
+        }
+    };
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -66,10 +98,12 @@ public class IntroViewport extends Viewport {
         goToGame = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
+        storyLineLabel = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(102, 0, 255));
 
         goToCharacterSelectionButton.setFont(new java.awt.Font("Yu Mincho", 3, 36)); // NOI18N
+        goToCharacterSelectionButton.setForeground(new java.awt.Color(240, 240, 240));
         goToCharacterSelectionButton.setText("Back");
         goToCharacterSelectionButton.setMaximumSize(new java.awt.Dimension(100, 100));
         goToCharacterSelectionButton.setMinimumSize(new java.awt.Dimension(59, 60));
@@ -89,8 +123,11 @@ public class IntroViewport extends Viewport {
         );
 
         goToGame.setFont(new java.awt.Font("Yu Mincho", 3, 36)); // NOI18N
+        goToGame.setForeground(new java.awt.Color(240, 240, 240));
         goToGame.setText("Continue");
         goToGame.setMaximumSize(new java.awt.Dimension(100, 60));
+
+        jPanel2.setOpaque(false);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -105,15 +142,23 @@ public class IntroViewport extends Viewport {
 
         jPanel3.setOpaque(false);
 
+        storyLineLabel.setFont(new java.awt.Font("Yu Mincho", 3, 36)); // NOI18N
+        storyLineLabel.setForeground(new java.awt.Color(240, 240, 240));
+        storyLineLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        storyLineLabel.setText("WELCOME");
+        storyLineLabel.setAutoscrolls(true);
+
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addComponent(storyLineLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 66, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addGap(0, 36, Short.MAX_VALUE)
+                .addComponent(storyLineLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -127,9 +172,9 @@ public class IntroViewport extends Viewport {
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(goToCharacterSelectionButton, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
+                        .addComponent(goToCharacterSelectionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 30, Short.MAX_VALUE)
                         .addGap(205, 205, 205)
-                        .addComponent(goToGame, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addComponent(goToGame, javax.swing.GroupLayout.DEFAULT_SIZE, 224, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -156,6 +201,7 @@ public class IntroViewport extends Viewport {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JLabel storyLineLabel;
     // End of variables declaration//GEN-END:variables
 
     @Override
