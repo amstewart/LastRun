4bb55f9a9cbd7da0e0dfedee57231ba3eb039480
amstewart <diacopium@gmail.com>
2015-04-19 14:53:03 -0400

    Fixed dev-alex branch

--- src/utility/DoubleLinkedList.java
+++ src/utility/DoubleLinkedList.java
@@ -3,8 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-<<<<<<< HEAD:depreciated/DoubleLinkedList.java
-package src.utility;
+package utility;
 
 /**
  *
